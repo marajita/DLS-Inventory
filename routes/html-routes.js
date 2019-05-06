7094750ffe83aef5e20baf15f4dc30cb43a13f1c
marajita <rajita.maharjan@gmail.com>
2019-05-05 20:50:38 -0400

    UI changes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,26 +8,50 @@ var path = require("path");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/dashboard.html"));
   });
 
   // cms route loads cms.html
-  app.get("/cms", function(req, res) {
+  app.get("/cms", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/cms.html"));
   });
 
   // blog route loads blog.html
-  app.get("/blog", function(req, res) {
+  app.get("/blog", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/blog.html"));
   });
 
   // authors route loads author-manager.html
-  app.get("/authors", function(req, res) {
+  app.get("/authors", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/author-manager.html"));
+  });
+
+    // admin
+    app.get("/admin", function (req, res) {
+      res.sendFile(path.join(__dirname, "../public/admin.html"));
+    });
+
+      // about
+  app.get("/about", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/about.html"));
+  });
+
+    // contact
+    app.get("/contact", function (req, res) {
+      res.sendFile(path.join(__dirname, "../public/contact.html"));
+    });
+  // template
+  app.get("/template", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/template.html"));
+  });
+
+  // template
+  app.get("/temp", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index5.html"));
   });
 };
